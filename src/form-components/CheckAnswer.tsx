63018198b0636241f941ca1540f589d7d3583ab6
vincenttucci <vincenttucci66@gmail.com>
2025-03-16 23:55:33 -0400

    checkanswer

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -5,9 +5,25 @@ export function CheckAnswer({
 }: {
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [answer, setAnswer] = useState("");
+
+    const addInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setAnswer(event.target.value);
+    };
+
+    const correct = () => {
+        if (answer === expectedAnswer) {
+            return "✔️";
+        } else {
+            return "❌";
+        }
+    };
+
     return (
         <div>
             <h3>Check Answer</h3>
+            <input type="text" value={answer} onChange={addInput} />
+            <div>{correct()}</div>
         </div>
     );
 }
